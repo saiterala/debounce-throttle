3c99dc29f02b75abf385e5b32dddf4c27433309c
Sai Terala <saikrishna.terala@kroger.com>
2022-02-21 17:23:56 -0500

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,80 @@
-import logo from './logo.svg';
+import React, {useRef, useState} from 'react'
 import './App.css';
 
-function App() {
+const App = () => {
+  const inputRef = useRef()
+  const [animals, setAnimals] = useState([])
+  const timerId = useRef()
+
+  const handleDebounceSearch = () => {
+    if(!inputRef.current.value){
+      setAnimals([])
+      return
+    }
+    clearTimeout(timerId.current)
+    timerId.current = setTimeout(() => {
+      clearTimeout(timerId.current)
+      fetch(`http://localhost:4000/animals?q=${inputRef.current.value}`)
+      .then(async(response, reject) => {
+        if(!response.ok){
+          reject('something went wrong')
+        }
+        const data = await response.json()
+        setAnimals(data)
+      })
+      .catch(err => {
+        console.log(err)
+      })
+    }, 700)
+    
+  }
+
+  const handleThrottleSearch = () => {
+    if(!inputRef.current.value){
+      setAnimals([])
+      return
+    }
+    if(timerId.current){
+      return
+    }
+    timerId.current = setTimeout(() => {
+      timerId.current = false
+      fetch(`http://localhost:4000/animals?q=${inputRef.current.value}`)
+      .then(async(response, reject) => {
+        if(!response.ok){
+          reject('something went wrong')
+        }
+        const data = await response.json()
+        setAnimals(data)
+      })
+      .catch(err => {
+        console.log(err)
+      })
+    }, 300)
+    
+  }
+
+  const items = animals.map(animal => (
+    <li key={animal.id}>
+      {animal.name}
+    </li>
+  ))
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <input
+        type='text'
+        ref={inputRef}
+        onChange={handleThrottleSearch}
+        className="search-input"
+      />
+        {inputRef.current?.value && animals.length > 0 && (
+          <ul>
+            {items}
+          </ul>
+        )}
+    
     </div>
-  );
+  )
 }
 
 export default App;
